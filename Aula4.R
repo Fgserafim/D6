46058038153f01ccd761df4b37d49587e169b817
Aluno Enap <aluno@enap.df>
2018-10-01 14:41:36 -0300

    Resolução do exercício 1 da aula 4 com nomeação do arquivo como juizes_drogas_CL

---
+++ Aula4.R
@@ -0,0 +1,40 @@
+#instalar pacotes
+lista.de.pacotes = c("tidyverse","lubridate") # escreva a lista de pacotes
+novos.pacotes <- lista.de.pacotes[!(lista.de.pacotes %in%
+                                      installed.packages()[,"Package"])]
+if(length(novos.pacotes) > 0) {install.packages(novos.pacotes)}
+lapply(lista.de.pacotes, require, character.only=T)
+rm(lista.de.pacotes,novos.pacotes)
+gc()
+
+#ler pacotes
+library(tidyverse)
+library(magrittr)
+library(lubridate)
+
+#ler arquivo decisões.rds
+decisoes <- read_rds("C:/Users/aluno.ENAP/Documents/FGS/D6/D6/decisoes.rds")
+
+#ver resumo do arquivo decisões
+glimpse(decisoes)
+
+# Criar objeto contendo o tempo medio entre decisoes e registro por juiz. Para isso:
+
+#1. Seleciona todas as colunas que serão utilizadas
+
+juizes_drogas_CL <- decisoes %>%
+  select(juiz, municipio, txt_decisao, data_registro, data_decisao) %>%
+  mutate(txt_decisao = tolower(txt_decisao),
+         droga = str_detect(txt_decisao,
+                            "droga|entorpecente|psicotr[óo]pico|maconha|haxixe|coca[íi]na"),
+         tempo = dmy(data_registro) - dmy(data_decisao)) %>%
+  filter(droga ==T, municipio %in% c("Campinas","Limeira")) %>%
+  group_by(juiz) %>%
+  summarise(tempo_medio = mean(tempo, na.rm=T))
+
+#salve o objeto resultante em um arquivo chamado "juizes_drogas_CL.rds"
+
+write_rds(juizes_drogas_CL, "C:/Users/aluno.ENAP/Documents/FGS/D6/D6/juizes_drogas_CL.rds)
+
+
+
